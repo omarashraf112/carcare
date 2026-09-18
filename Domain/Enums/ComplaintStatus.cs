c89843aa430cb4083a7faaebeef70401b3d84f22
omar <omarashraf20060118@gmail.com>
2026-09-19 01:13:03 +0300

    update entities

---
+++ Domain/Enums/ComplaintStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Enums
+{
+    public enum ComplaintStatus
+    {
+        Open,
+        UnderReview,
+        Resolved,
+        Rejected
+    }
+}
